0b61c1569b70308929997029705297d0f5f53cef
michaelshamrell <michaelshamrell2001@gmail.com>
2022-01-12 14:24:31 -0800

    comfigured scripts and html to fit new calculator standards.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,26 @@
+// Business logic:
+
 function add(number1, number2) {
   return number1 + number2;
 }
+
 function subtract(number1, number2) {
   return number1 - number2;
 }
+
 function multiply(number1, number2) {
-  return number1 * number2; 
+  return number1 * number2;
 }
+
 function divide(number1, number2) {
   return number1 / number2;
 }
 
+// Everything below this line is user interface logic:
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = subtract(number1, number2);
-alert(result);
+$(document).ready(function() {
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+
+  alert(add(number1, number2));
+});
